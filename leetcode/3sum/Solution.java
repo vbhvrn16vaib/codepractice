764a01323132c0fedfa375af0732b731530a5d40
vaibhav rana <vbhvrn@gmail.com>
2022-02-10 23:53:27 +0100

    leetcode problem addition

---
+++ leetcode/3sum/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for(int i =0; i < nums.length; i++){
+            map.putIfAbsent(nums[i],new HashSet<>());
+            map.get(nums[i]).add(i);
+        }
+        Set<List<Integer>> ps = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                int sum = nums[i] + nums[j];
+                System.out.println(map);
+                if(map.containsKey(0-sum) && !map.get(0-sum).contains(i) && !map.get(0-sum).contains(j)){
+                    List<Integer> d = Arrays.asList(nums[i],nums[j],0-sum);
+                    Collections.sort(d);
+                    ps.add(d);
+                }
+            }
+        }
+        return new ArrayList<>(ps);
+    }
+}
