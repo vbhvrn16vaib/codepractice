062e536e61090c8f004bd3895c1675707cbe02ad
vaibhav rana <vbhvrn@gmail.com>
2022-02-14 10:55:59 +0100

    leetcode problem addition

--- leetcode/maximum-length-of-repeated-subarray/Solution.java
+++ leetcode/maximum-length-of-repeated-subarray/Solution.java
@@ -1,23 +1,13 @@
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
-        Map<Integer, List<Integer>> posMap = new HashMap<>();
-        for(int i = 0 ; i < nums2.length; i++){
-            posMap.putIfAbsent(nums2[i], new ArrayList<>());
-            posMap.get(nums2[i]).add(i);
-        }
-        int max = 0;
-        for(int i = 0; i < nums1.length; i++){
-            List<Integer> pos2 = posMap.getOrDefault(nums1[i], new ArrayList<>());
-            for(int p : pos2){
-                int j = i;
-                int k = p;
-                int cnt = 0;
-                while( j < nums1.length && k < nums2.length && nums1[j] == nums2[k]){
-                    j+=1;
-                    k+=1;
-                    cnt+=1;
+        int max =0;
+        int[][] dp = new int[nums1.length+1][nums2.length+1];
+        for(int i = nums1.length-1; i>=0 ; i--){
+            for(int j = nums2.length-1; j>=0 ; j--){
+                if(nums1[i] == nums2[j]){
+                    dp[i][j] = dp[i+1][j+1]+1;
+                    max =Math.max(dp[i][j],max);
                 }
-                max = Math.max(cnt, max);
             }
         }
         return max;
