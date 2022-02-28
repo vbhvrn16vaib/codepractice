42d732011add6cdb666813d93bb222755cea2a6e
vaibhav rana <vbhvrn@gmail.com>
2022-02-28 22:27:11 +0100

    leetcode problem addition

--- leetcode/shortest-distance-from-all-buildings/Solution.java
+++ leetcode/shortest-distance-from-all-buildings/Solution.java
@@ -1,70 +1,39 @@
-class Solution {
-    public int shortestDistance(int[][] grid) {
-        int[][] fdp = new int[grid.length][grid[0].length];
-        Queue<int[]> que = new LinkedList<>();
-        
-        for(int i =0 ; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1){
-                    Integer[][] dp = new Integer[grid.length][grid[0].length];
-                    dfs(grid, i, j, dp);
-                   
-                    // for(int k = 0; k < dp.length; k++) System.out.println(Arrays.toString(dp[k]));
-                    sum(dp, fdp);
-                }
-            }
-        }
-        
-        // for(int k = 0; k < fdp.length; k++) System.out.println(Arrays.toString(fdp[k]));
-        int min  = Integer.MAX_VALUE;
-        for(int k = 0; k < fdp.length; k++){
-            for(int f = 0; f < fdp[0].length; f++){
-                min = Math.min(min, fdp[k][f]);
-            }
-        }
-    
-        return min == Integer.MAX_VALUE ? -1 : min;
-        
-    }
-    int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
-    
-    void dfs(int[][] grid, int i , int j,Integer[][] dp){
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{i,j,0});
-        grid[i][j] = 0;
-        boolean[][] visited = new boolean[dp.length][dp[0].length];
-        while(!que.isEmpty()){
-            int[] v = que.poll();
-            int x = v[0];
-            int y = v[1];
-            int sf = v[2];
-            visited[x][y] = true;
-            for(int[] dir : dirs){
-                int x1 = x + dir[0];
-                int x2 = y + dir[1];
-                if(isNotValid(grid, x1, x2, visited)){
-                    continue;
-                }
-                dp[x1][x2] = sf+1;
-                que.add(new int[]{x1,x2,sf+1});
-            }
-        }
-        grid[i][j] = 1;
-    }
-    
-    boolean isNotValid(int[][] grid, int i, int j, boolean[][] visited){
-        return i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j] == 1 || grid[i][j] == 2 || visited[i][j];
-    }
-    
-    void sum(Integer[][] dp, int[][] fdp){
-        for(int i =0 ; i < dp.length; i++){
-            for(int j = 0; j < dp[0].length; j++){
-                if (fdp[i][j] == Integer.MAX_VALUE || dp[i][j] == null) {
-                    fdp[i][j]=Integer.MAX_VALUE;
-                    continue;
-                }
-                fdp[i][j]+=dp[i][j];
-            }
-        }
-    }
+                }
+                visited[i][j] = new HashSet<>();
+            }
+        }
+        while(!que.isEmpty()){
+            int[] p = que.poll();
+            int x = p[0];
+            int y = p[1];
+            int l = p[2];
+            int id = p[3];
+            if(visited[x][y].contains(id))continue;
+            visited[x][y].add(id);
+            if(visited[x][y].size() == maxcnt){
+//                 for(int i = 0 ; i < dp.length; i++){
+//                     for(int j = 0; j < dp[0].length; j++){
+//                         System.out.print(visited[i][j]);
+//                     }
+//                     System.out.println("--");
+//                 }
+                for(int i = 0; i < dp.length; i++) System.out.println(Arrays.toString(dp[i]));
+                return dp[x][y];
+            }
+            for(int[] dir : dirs){
+                int x1 = x + dir[0];
+                int y1 = y + dir[1];
+                if(!isNotValid(grid, x1, y1) && !visited[x1][y1].contains(id)){
+                    dp[x1][y1] += l+1;
+                    que.add(new int[]{x1,y1,l+1,id});
+                }
+            }
+        }
+        // for(int i = 0; i < dp.length; i++) System.out.println(Arrays.toString(dp[i]));
+        return -1;
+    }
+    int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+    boolean isNotValid(int[][] grid, int i, int j){
+        return i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j] == 1 || grid[i][j] == 2;
+    }
 }
